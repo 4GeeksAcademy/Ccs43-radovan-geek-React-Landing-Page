fe6ad876000e0ff4e3837e59082ff1cc847fe1a4
Radovan-geek <131044960+Radovan-geek@users.noreply.github.com>
2023-06-21 23:57:33 +0000

    actualizacion

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Navbar = () => {
-  <div></div>;
+  <div><nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div classname="container-fluid">
+    <a classname="navbar-brand" href="#">START BOOTSTRAP</a>
+    <button classname="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span classname="navbar-toggler-icon"></span>
+    </button>
+    <div classname="collapse navbar-collapse" id="navbarNav">
+      <ul classname="navbar-nav">
+        <li classname="nav-item">
+          <a classname="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li classname="nav-item">
+          <a classname="nav-link" href="#">About</a>
+        </li>
+        <li classname="nav-item">
+          <a classname="nav-link" href="#">Service</a>
+        </li>
+        <li classname="nav-item">
+          <a classname="nav-link"href="#">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav></div>;
 };
 
 export default Navbar;
