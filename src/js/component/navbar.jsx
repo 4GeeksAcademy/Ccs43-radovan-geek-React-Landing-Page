452935f0701e325958b2604c09f8989b08c6a45b
Radovan-geek <131044960+Radovan-geek@users.noreply.github.com>
2023-06-23 13:10:59 +0000

    navbar arrangement

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-dark">
-      <div className="container-fluid navbar w-75 p-2">
+      <div className="container-fluid navbar w-75">
         <a className="navbar-brand text-end" style={{ color: 'white' }} href="#">START BOOTSTRAP</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
